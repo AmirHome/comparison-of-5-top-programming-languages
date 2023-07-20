8cd884262e34120a508e65cc5455de2af50b8987
AmirHome <amir.email@yahoo.com>
2023-07-20 11:06:31 +0300

    Fixed

--- java/02-boolean.java
+++ java/02-boolean.java
@@ -1,21 +1,20 @@
-public class AnnalynsInfiltration {
-    // Task 1: Check if a fast attack can be made
+class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
         return !knightIsAwake;
     }
 
-    // Task 2: Check if the group can be spied upon
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        return !knightIsAwake && archerIsAwake && !prisonerIsAwake;
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
-    // Task 3: Check if the prisoner can be signalled
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
         return !archerIsAwake && prisonerIsAwake;
     }
 
-    // Task 4: Check if the prisoner can be freed
-    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        return !knightIsAwake && archerIsAwake && !prisonerIsAwake && petDogIsPresent;
+    public static boolean canFreePrisoner(boolean knightIsAwake, 
+                                          boolean archerIsAwake, 
+                                          boolean prisonerIsAwake, 
+                                          boolean petDogIsPresent) {
+        return !knightIsAwake && !archerIsAwake && prisonerIsAwake || !archerIsAwake && petDogIsPresent;
     }
 }
--- javascript/02-boolean.js
+++ javascript/02-boolean.js
@@ -1,25 +1,54 @@
+/**
+ * The fast attack is available when the knight is sleeping
+ *
+ * @param {boolean} knightIsAwake
+ *
+ * @return {boolean} Whether or not you can execute a fast attack.
+ */
 export function canExecuteFastAttack(knightIsAwake) {
-  // Fast attack is possible if the knight is not awake (sleeping).
   return !knightIsAwake;
 }
 
+/**
+ * A useful spy captures information, which they can't do if everyone's asleep.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can spy on someone.
+ */
 export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
-  // Spy action is possible if at least one of the characters (knight, archer, prisoner) is awake.
   return knightIsAwake || archerIsAwake || prisonerIsAwake;
 }
 
+/**
+ * You'll get caught by the archer if you signal while they're awake.
+ *
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can send a signal to the prisoner.
+ */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  // Signal Prisoner action is possible if the archer is not awake (sleeping).
-  return !archerIsAwake;
+  return !archerIsAwake && prisonerIsAwake;
 }
 
+/**
+ * The final stage in the plan: freeing Annalyn's best friend.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ * @param {boolean} petDogIsPresent
+ *
+ * @returns {boolean} Whether or not you can free Annalyn's friend.
+ */
 export function canFreePrisoner(
   knightIsAwake,
   archerIsAwake,
   prisonerIsAwake,
   petDogIsPresent
 ) {
-  // Free Prisoner action is possible if the knight is not awake (sleeping),
-  // the archer is not awake (sleeping), and the prisoner is awake or Annalyn's pet dog is present.
-  return !knightIsAwake && !archerIsAwake && (prisonerIsAwake || petDogIsPresent);
+  return (petDogIsPresent && !archerIsAwake) || (prisonerIsAwake && !knightIsAwake && !archerIsAwake);
 }
